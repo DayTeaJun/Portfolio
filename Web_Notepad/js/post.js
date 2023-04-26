ef89a8fc5f09f5b766eab945a6ecfb05b7b08c92
DayTeaJun <gkfnck@gmail.com>
2023-04-26 22:52:24 +0900

    mediaQuery edit

--- Web_Notepad/js/post.js
+++ Web_Notepad/js/post.js
@@ -1,15 +1,15 @@
-const inpTitle = document.querySelector('.inp-title'); // 제목
-const contentText = document.querySelector('#content-textarea'); // 내용
-const inpBtn = document.querySelector('.inp-btn'); // 버튼
+const inpTitle = document.querySelector('.inp-title');
+const contentText = document.querySelector('#content-textarea');
+const inpBtn = document.querySelector('.inp-btn');
 const today = new Date();
+const fileInput = document.querySelector(".inp-file");
 
+const uploadNameInput = document.querySelector('.upload-name');
 
-// 로컬스토리지에 저장
 let allPost = JSON.parse(localStorage.getItem("allPost"));
 allPost = allPost ?? [];
 render();
 
-// 제출하기 버튼 클릭시 로컬스토리지에 저장
 inpBtn.addEventListener('click', function (e) {
     e.preventDefault();
 
@@ -20,11 +20,15 @@ inpBtn.addEventListener('click', function (e) {
     else {
         const title = inpTitle.value;
         const content = contentText.value;
-        allPost.push({ title, content, len: allPost.length });
+        const date = today.toISOString().slice(0, 10)
+
+        allPost.push({ title, content, date, len: allPost.length });
         localStorage.setItem("allPost", JSON.stringify(allPost));
         inpTitle.value = "";
         contentText.value = "";
-        render()
+        uploadNameInput.value = "";
+        render();
+
     }
 })
 
@@ -32,7 +36,6 @@ function render() {
 
     const articleContainer = document.querySelector('.article-container');
     articleContainer.innerHTML = "";
-    // const frag = document.createDocumentFragment(); // 가상 돔 만듦
     let count = 0;
 
     for (const item of allPost) {
@@ -49,7 +52,7 @@ function render() {
 
         const cardImg = document.createElement('img');
         cardImg.setAttribute('class', 'img');
-        cardImg.setAttribute('src', `./img/cat1.jpg`);
+        cardImg.setAttribute('src', './img/cat1.jpg');
         cardBorder.appendChild(cardImg);
 
         const cardText = document.createElement('div');
@@ -78,7 +81,7 @@ function render() {
         const underline = document.createElement('p');
         underline.setAttribute('class', 'underline');
         cardFooter.appendChild(underline);
-        underline.textContent = today.toISOString().slice(0, 10);
+        underline.textContent = item.date;
 
         const deletePostBtn = document.createElement('button');
         deletePostBtn.setAttribute('class', 'close');
@@ -87,7 +90,6 @@ function render() {
         deletePostBtn.setAttribute('onclick', 'remove()');
         cardItem.appendChild(deletePostBtn);
         deletePostBtn.textContent = '✖️'
-        // img 넣고 싶은데 넣으면 에러가 뜸 (수정예정)
     }
 
 }
@@ -105,12 +107,18 @@ function remove() {
     render();
 }
 
-function postLink() {
-    alert('게시물 구현이 아직 준비되지 않았습니다!');
-}
-
 function deleteAll() {
     allPost.splice(0);
     localStorage.setItem("allPost", JSON.stringify(allPost));
     render();
 }
+
+function postLink() {
+    alert('게시물 구현이 아직 준비되지 않았습니다!');
+}
+
+fileInput.addEventListener('change', function () {
+    const fileName = fileInput.value.split('\\').pop();
+    uploadNameInput.value = fileName;
+    alert('로컬스토리지에 일정 이상의 이미지파일을 넣으면 크기가 너무 커서\n에러가 발생합니다.\n구현예정입니다.');
+});
